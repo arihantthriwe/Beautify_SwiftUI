329b0e9ceb9aa8acc19d741b2496c7a5c02cfaf9
Arihant Thriwe <arihant.jain@thriwe.com>
2021-12-16 10:49:31 +0530

    Initial Commit

---
+++ Beautify_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Beautify_SwiftUI
+//
+//  Created by Arihant Thriwe on 16/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
