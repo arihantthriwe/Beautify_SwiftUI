5c6806e79700da196148b92869fba5af7c6f2584
Arihant Thriwe <arihant.jain@thriwe.com>
2021-12-16 11:07:39 +0530

    Initial Setup

--- Beautify_SwiftUI/ContentView.swift
+++ Beautify_SwiftUI/ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Text("Target Color Block")
+        }
     }
 }
 
